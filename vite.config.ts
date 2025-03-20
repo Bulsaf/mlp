3a34b0d73b8474453a8a971b53409b1e75b5520c
Bulat <bulatsafiullin2612@gmail.com>
2025-03-20 12:20:17 +0300

    Удален адрес домашней страницы из package.json. Исправлен базовый путь в конфигурации Vite для корректной работы проекта.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
-	base:"/mlp/"
+	base:'/mlp',
+	plugins: [react(), tailwindcss()]
 });
